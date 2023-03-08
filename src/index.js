531e12ddd6dba5b83c08e6c47c36754da7dc4f1a
Sakthiveel <sakthivelsaravanan636@gmail.com>
2023-03-08 20:20:29 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import App from './App';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-alert("Application not yet optimized to small screens. Kindly visit thorough laptop or pc.");
+
 root.render(<App />);
